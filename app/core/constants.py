0290914d107ee67980906ae06886ee784e9aaa20
Ale007XD <alex.deloverov@gmail.com>
2026-03-12 09:20:34 +0800

    Create constants.py

---
+++ app/core/constants.py
@@ -0,0 +1,12 @@
+# app/core/constants.py
+
+from datetime import time
+
+SLOT_INTERVAL_MINUTES = 60
+BASE_BUFFER_MINUTES = 30
+
+OPENING_TIME = time(9, 0)
+CLOSING_TIME = time(21, 0)
+
+MAX_MENU_DEPTH = 3
+DEFAULT_LIMIT = 100
