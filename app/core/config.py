b66757c3cea3ca5e5b84435dceb48d9a1484199b
Ale007XD <alex.deloverov@gmail.com>
2026-03-12 09:21:40 +0800

    Update config.py

--- app/core/config.py
+++ app/core/config.py
@@ -1,1 +1,17 @@
 # app/core/config.py
+
+from pydantic import BaseSettings
+
+
+class Settings(BaseSettings):
+
+    APP_NAME: str = "Sieshka API"
+
+    DATABASE_URL: str = "sqlite:///./app.db"
+
+    API_PREFIX: str = "/api"
+
+    DEBUG: bool = True
+
+
+settings = Settings()
